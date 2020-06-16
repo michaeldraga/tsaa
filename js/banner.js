c1e9bc384e6452cdc31cbdc7999457f54b3cbf63
Michael Draga (Surface) <midall01.md@gmail.com>
2020-06-16 15:48:43 +0200

    added the covid19 banner everywhere and wrote a script that lets the cookie expire instantly

--- js/banner.js
+++ js/banner.js
@@ -1,1 +1,4 @@
-window.addEventListener('load', () => document.getElementsByClassName('wpcc-btn')[0].href = '/files/Hygienekonzept Tanz Sport Academy Allround e.V.pdf');
+window.addEventListener('load', () => {
+    document.getElementsByClassName('wpcc-btn')[0].href = '/files/Hygienekonzept Tanz Sport Academy Allround e.V.pdf';
+    document.cookie = document.cookie + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+})
