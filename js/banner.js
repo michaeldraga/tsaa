0c4fe0ce1e34e1702e4537f6bfb01cae0be74ba2
Michael Draga (Surface) <midall01.md@gmail.com>
2020-06-26 00:03:57 +0200

    fixed Hygienebanner cookies and added cheerdance flyer

--- js/banner.js
+++ js/banner.js
@@ -1,4 +1,13 @@
-window.addEventListener('load', () => {
-    document.getElementsByClassName('wpcc-btn')[0].href = '/files/Hygienekonzept Tanz Sport Academy Allround e.V.pdf';
-    document.cookie = document.cookie + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-})
+const minutes = 60000;
+
+window.addEventListener("load", () => {
+  document.getElementsByClassName("wpcc-btn")[0].href =
+    "/files/Hygienekonzept Tanz Sport Academy Allround e.V.pdf";
+  const now = new Date();
+  const fiveMinutes = new Date(now.getTime() + 2 * minutes);
+  console.log(now.getTime());
+  // document.cookie = document.cookie + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  console.log("UTC: " + fiveMinutes.toUTCString());
+  document.cookie =
+    document.cookie + `;expires=${fiveMinutes.toUTCString()};path=/;`;
+});
